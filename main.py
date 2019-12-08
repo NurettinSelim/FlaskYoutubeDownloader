c27ae29a947ebdc4f507d36791ccef220d0972d5
NurettinS <nurettinselim@noreply.github.com>
2019-12-08 11:57:23 +0300

    new version

--- main.py
+++ main.py
@@ -2,15 +2,26 @@
 #Kullanılacak Modüller
 from flask import Flask,render_template,request,redirect,send_from_directory,abort
 from pytube import YouTube
+import os
 
 app = Flask(__name__)
 
 #Sorun çıkartan karakterler dosya adından silen foksiyon
-def temizle(kod): 
+def temizle3(kod): 
     kod = kod.replace("|","")
     kod = kod.replace(".","")
     kod = kod.replace('"',"")
     kod = kod.replace(':',"")
+    kod = kod.replace('#',"")
+    kod = kod[:-3]
+    kod = kod + ".mp3"
+    return kod
+def temizle4(kod): 
+    kod = kod.replace("|","")
+    kod = kod.replace(".","")
+    kod = kod.replace('"',"")
+    kod = kod.replace(':',"")
+    kod = kod.replace('#',"")
     kod = kod[:-3]
     kod = kod + ".mp4"
     return kod
@@ -35,16 +46,22 @@ def index():
 @app.route("/download/<string:id>")
 def download(id):    
     video = YouTube("http://youtube.com/watch?v="+id)
-    video_name = video.title        
-    video.streams.filter(progressive=True,file_extension='mp4').first().download("FlaskYoutubeDownloader/download/")
-    
+    video_name = video.title 
+    video_name = video_name+".mp4"       
+    video.streams.filter(only_audio=True,file_extension='mp4').first().download("down/")
 
-    return render_template("start.html",title = video_name)
+    name4= temizle4(video_name)
+    name3= temizle3(video_name)
+
+    code= """ ffmpeg -i "D:\Python Projeleri\FlaskYoutubeDownloader\down\{}" -f mp3 -ab 128000 -vn "D:\Python Projeleri\FlaskYoutubeDownloader\down\{}" """.format(name4,name3)
+    print(code)
+    os.system(code)
+    return render_template("start.html",title = video_name[:-3])
 
 @app.route("/file/<string:name>")
 def file(name):
-    print(temizle(name))
-    return send_from_directory("FlaskYoutubeDownloader/download/",temizle(name),as_attachment=True)
+    print(temizle3(name))
+    return send_from_directory("down/",temizle3(name),as_attachment=True)
 
 
 if __name__ == "__main__":
